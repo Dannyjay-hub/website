2641faee7e995f30bdd42b5d2f2c3f29b4ff9ee6
Daniel Jesusegun <65185267+Dannyjay-hub@users.noreply.github.com>
2026-07-02 19:58:49 +0100

    Remove call column from footer, adjust column sizes to 6, resolve copyright margin-collapse gap, move partners header outside and decrease vertical gap, use inline onclick Calendly triggers

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,7 +44,7 @@ Assigned to: ThemeForest
 			this.window_scroll();
 			this.scroll_contact();
 			this.read_more();
-			this.calendly_popup();
+			
 			
 		},
 		
@@ -467,19 +467,7 @@ Assigned to: ThemeForest
 	},
 	/*------------------------------------------------------------------*/ 
 	
-	// Calendly Popup Integration
-	calendly_popup: function() {
-		$(document).on('click', 'a[href*="calendly.com"]', function(e) {
-			e.preventDefault();
-			if (typeof Calendly !== 'undefined') {
-				Calendly.initPopupWidget({ url: $(this).attr('href') });
-			} else {
-				// Fallback to opening in new window if script fails to load
-				window.open($(this).attr('href'), '_blank');
-			}
-			return false;
-		});
-	},
+	
 	/*------------------------------------------------------------------*/ 
 	
 	};
